2347fe7f8553f37669047eb2a02cc6e9534019ce
Matthew Benton <m@mattb.tech>
2020-08-24 21:00:36 +0100

    Start to style index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,12 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <>
-      <Link href="/[sketchName]/[seed]" as="/1/test">
-        <a>1</a>
-      </Link>
-    </>
+    <div className="layout">
+      <div className="sketch">
+        <Link href="/[sketchName]/[seed]" as="/1/test">
+          <a>1</a>
+        </Link>
+      </div>
+    </div>
   );
 }
