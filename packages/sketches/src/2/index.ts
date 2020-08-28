d8d955f73428c9ed2050d931d22f9b3bc564d804
Matthew Benton <m@mattb.tech>
2020-08-28 18:59:02 +0100

    Sketch 2

---
+++ packages/sketches/src/2/index.ts
@@ -0,0 +1,131 @@
+import sign from "@mattb.tech/graphique-sign";
+import random, { resetRandomness } from "@mattb.tech/graphique-random";
+import { polygonCentroid } from "geometric";
+
+const SKETCH_ID = 2;
+
+const DIVISION_SIZE_X = 200;
+const DIVISION_SIZE_Y = 200;
+const POINT_BOX_MARGIN = 10;
+
+function subdivideSpace(
+  { width: areaWidth, height: areaHeight }: { width: number; height: number },
+  {
+    width: subdivisionWidth,
+    height: subdivisionHeight,
+  }: { width: number; height: number },
+  margin: number = 0
+): {
+  top: number;
+  left: number;
+  bottom: number;
+  right: number;
+  width: number;
+  height: number;
+}[] {
+  const numberX = Math.floor(areaWidth / subdivisionWidth);
+  const numberY = Math.floor(areaHeight / subdivisionHeight);
+  const outerMarginX = (areaWidth % subdivisionWidth) / 2;
+  const outerMarginY = (areaHeight % subdivisionHeight) / 2;
+  return [...Array(numberY).keys()].flatMap((y) =>
+    [...Array(numberX).keys()].map((x) => ({
+      top: outerMarginY + margin + subdivisionHeight * y,
+      bottom: outerMarginY - margin + subdivisionHeight * (y + 1),
+      left: outerMarginX + margin + subdivisionWidth * x,
+      right: outerMarginX - margin + subdivisionWidth * (x + 1),
+      width: subdivisionWidth,
+      height: subdivisionHeight,
+    }))
+  );
+}
+
+export function sketch({
+  canvas,
+  seed,
+}: {
+  canvas: HTMLCanvasElement;
+  seed: string;
+}) {
+  resetRandomness(seed);
+  const ctx: CanvasRenderingContext2D = canvas.getContext("2d")!;
+
+  // Off-white background
+  ctx.fillStyle = `hsl(${random.scaled(0, 360)}, 40%, 97%)`;
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  ctx.strokeStyle = `hsl(${random.scaled(0, 360)}, 60%, 70%)`;
+  subdivideSpace(canvas, {
+    width: DIVISION_SIZE_X,
+    height: DIVISION_SIZE_Y,
+  }).forEach(({ top, left, bottom, right, width, height }) => {
+    const topLeft: [number, number] = [
+      random.scaled(
+        left + POINT_BOX_MARGIN,
+        left + width / 2 - POINT_BOX_MARGIN
+      ),
+      random.scaled(
+        top + POINT_BOX_MARGIN,
+        top + height / 2 - POINT_BOX_MARGIN
+      ),
+    ];
+    const topRight: [number, number] = [
+      random.scaled(
+        left + width / 2 + POINT_BOX_MARGIN,
+        right - POINT_BOX_MARGIN
+      ),
+      random.scaled(
+        top + POINT_BOX_MARGIN,
+        top + height / 2 - POINT_BOX_MARGIN
+      ),
+    ];
+    const bottomRight: [number, number] = [
+      random.scaled(
+        left + width / 2 + POINT_BOX_MARGIN,
+        right - POINT_BOX_MARGIN
+      ),
+      random.scaled(
+        top + height / 2 + POINT_BOX_MARGIN,
+        bottom - POINT_BOX_MARGIN
+      ),
+    ];
+    const bottomLeft: [number, number] = [
+      random.scaled(
+        left + POINT_BOX_MARGIN,
+        left + width / 2 - POINT_BOX_MARGIN
+      ),
+      random.scaled(
+        top + height / 2 + POINT_BOX_MARGIN,
+        bottom - POINT_BOX_MARGIN
+      ),
+    ];
+    ctx.beginPath();
+    ctx.lineTo(...topLeft);
+    ctx.lineTo(...topRight);
+    ctx.lineTo(...bottomRight);
+    ctx.lineTo(...bottomLeft);
+    ctx.closePath();
+    ctx.stroke();
+
+    const center = polygonCentroid([
+      topLeft,
+      topRight,
+      bottomRight,
+      bottomLeft,
+      topLeft,
+    ]);
+
+    ctx.beginPath();
+    ctx.lineTo(...topLeft);
+    ctx.lineTo(...center);
+    ctx.lineTo(...bottomRight);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.lineTo(...bottomLeft);
+    ctx.lineTo(...center);
+    ctx.lineTo(...topRight);
+    ctx.stroke();
+  });
+
+  sign(SKETCH_ID, seed)(ctx);
+}
