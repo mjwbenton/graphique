6bdb14b61ca9baacf0b728e8e075bb86aba14100
Matthew Benton <m@mattb.tech>
2020-08-28 19:00:13 +0100

    Add sketch 2 to index

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,13 @@ export default function Home() {
   return (
     <div className="layout">
       <div className="sketch">
-        <Link href="/[sketchName]/[seed]" as="/1/test">
+        <Link href="/[sketchName]/[seed]" as="/1/1">
           <a>1</a>
+        </Link>
+      </div>
+      <div className="sketch">
+        <Link href="/[sketchName]/[seed]" as="/2/2">
+          <a>2</a>
         </Link>
       </div>
     </div>
