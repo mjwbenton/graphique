0776bc323b730bd1a2ab9fb002fdf024da8f78ff
Matthew Benton <m@mattb.tech>
2021-03-11 09:41:27 +0000

    Increase size of sign

--- packages/sign/src/index.ts
+++ packages/sign/src/index.ts
@@ -4,7 +4,7 @@ export default function sign(
 ): (ctx: CanvasRenderingContext2D) => void {
   return (ctx: CanvasRenderingContext2D) => {
     const sign = `mattb / ${sketchId} / ${seed}`;
-    ctx.font = "14px Consolas, Monaco, monospace";
+    ctx.font = "28px Consolas, Monaco, monospace";
     ctx.fillStyle = "rgba(0,0,0,0.3)";
     const { width } = ctx.measureText(sign);
     ctx.fillText(sign, ctx.canvas.width - (20 + width), ctx.canvas.height - 20);
