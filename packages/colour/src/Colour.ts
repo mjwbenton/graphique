0594f651807f876a6c9b333a191f45f4d120626b
Matthew Benton <m@mattb.tech>
2021-03-15 15:10:39 +0000

    Add support for opacity to Colour

--- packages/colour/src/Colour.ts
+++ packages/colour/src/Colour.ts
@@ -4,17 +4,20 @@ export interface ColourParams {
   readonly hue: number;
   readonly saturation: number;
   readonly lightness: number;
+  readonly opacity: number;
 }
 
 export default class Colour {
   private readonly hue: number;
   private readonly saturation: number;
   private readonly lightness: number;
+  private readonly opacity: number;
 
-  constructor(params: ColourParams) {
+  constructor(params: Partial<ColourParams> & Pick<ColourParams, "hue">) {
     this.hue = params.hue;
-    this.saturation = params.saturation;
-    this.lightness = params.lightness;
+    this.saturation = params.saturation ?? 50;
+    this.lightness = params.lightness ?? 50;
+    this.opacity = params.opacity ?? 1;
     if (this.hue > 360 || this.hue < 0) {
       throw new Error(`Invalid hue ${this.hue}`);
     }
@@ -24,10 +27,17 @@ export default class Colour {
     if (this.saturation > 100 || this.saturation < 0) {
       throw new Error(`Invalid saturation ${this.saturation}`);
     }
+    if (this.opacity > 1 || this.opacity < 0) {
+      throw new Error(`Invalid opacity ${this.opacity}`);
+    }
   }
 
   toHSL(): string {
-    return `hsl(${this.hue}, ${this.saturation}%, ${this.lightness}%)`;
+    if (this.opacity != 1) {
+      return `hsla(${this.hue}, ${this.saturation}%, ${this.lightness}%, ${this.opacity})`;
+    } else {
+      return `hsl(${this.hue}, ${this.saturation}%, ${this.lightness}%)`;
+    }
   }
 
   override(params: Partial<ColourParams>): Colour {
@@ -35,6 +45,7 @@ export default class Colour {
       hue: params.hue ?? this.hue,
       saturation: params.saturation ?? this.saturation,
       lightness: params.lightness ?? this.lightness,
+      opacity: params.opacity ?? this.opacity,
     });
   }
 
@@ -68,6 +79,18 @@ export default class Colour {
     });
   }
 
+  increaseOpacity(amount: number): Colour {
+    return this.override({
+      opacity: addOpacity(this.opacity, amount),
+    });
+  }
+
+  decreaseOpacity(amount: number): Colour {
+    return this.override({
+      opacity: addOpacity(this.opacity, amount * -1),
+    });
+  }
+
   createPaletteWith(
     ...createFuncs: ReadonlyArray<(base: Colour) => Colour>
   ): Palette {
@@ -91,5 +114,9 @@ function addHue(hue: number, add: number) {
 }
 
 function addPercent(existing: number, add: number) {
-  return Math.max(0, Math.min(existing + add, 100));
+  return Math.max(0, Math.min(100, existing + add));
+}
+
+function addOpacity(existing: number, add: number) {
+  return Math.max(0, Math.min(1, existing + add));
 }
