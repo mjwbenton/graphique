ff726827b2e3480014ec77b581f4e4dac06d44d0
Matthew Benton <m@mattb.tech>
2021-05-22 19:36:01 +0100

    Improved command line interface for print

--- packages/print/src/bin/print.ts
+++ packages/print/src/bin/print.ts
@@ -1,26 +1,53 @@
 #!/usr/bin/env node
 
+import {
+  command,
+  positional,
+  binary,
+  run,
+  option,
+  number,
+  string,
+} from "cmd-ts";
 import { createCanvas } from "canvas";
 import { createWriteStream } from "fs";
 
-const CANVAS_SIZE_X = 3000;
-const CANVAS_SIZE_Y = 2400;
+const DEFAULT_W = 3000;
+const DEFAULT_H = 3000;
 
-const canvas = createCanvas(CANVAS_SIZE_X, CANVAS_SIZE_Y);
+const app = command({
+  name: "graphique-print",
+  args: {
+    sketch: positional({ type: string }),
+    seed: positional({ type: string }),
+    width: option({
+      type: number,
+      long: "width",
+      short: "w",
+      defaultValue: () => DEFAULT_W,
+    }),
+    height: option({
+      type: number,
+      long: "height",
+      short: "h",
+      defaultValue: () => DEFAULT_H,
+    }),
+  },
+  async handler({ sketch: sketchName, seed, width, height }) {
+    const canvas = createCanvas(width, height);
+    const { sketch } = await import(
+      `@mattb.tech/graphique-sketches/sketches/${sketchName}`
+    );
+    sketch({
+      canvas,
+      seed,
+      createCanvas,
+    });
+    const filename = `graphique-${sketchName}-${seed}.png`;
+    const output = createWriteStream(filename);
+    canvas.createPNGStream().pipe(output);
+    output.on("finish", () => console.log(filename));
+  },
+});
 
-const [sketchName, seed] = process.argv.splice(2);
-async function main() {
-  const { sketch } = await import(
-    `@mattb.tech/graphique-sketches/sketches/${sketchName}`
-  );
-  sketch({
-    canvas,
-    seed,
-    createCanvas,
-  });
-  const filename = `graphique-${sketchName}-${seed}.png`;
-  const output = createWriteStream(filename);
-  canvas.createPNGStream().pipe(output);
-  output.on("finish", () => console.log(filename));
-}
-main();
+run(binary(app), process.argv);
