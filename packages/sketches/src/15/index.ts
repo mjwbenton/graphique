dc4ef3bfcc39025883572058704b53fc9c1d9e4f
Matthew Benton <m@mattb.tech>
2022-11-23 16:25:28 +0000

    Tweaked 15

--- packages/sketches/src/15/index.ts
+++ packages/sketches/src/15/index.ts
@@ -9,8 +9,8 @@ export const sketch: Sketch = async ({ canvas, seed }) => {
   const ctx: CanvasRenderingContext2D = canvas.getContext("2d")!;
 
   const sideLength = random.scaled(30, 100);
-  const yDistance = sideLength * random.scaled(0.2, 0.6);
-  const xDistance = sideLength * random.scaled(1.1, 3.0);
+  const yDistance = sideLength * random.scaled(0.6, 1.0);
+  const xDistance = sideLength * random.scaled(3.0, 5.0);
   const rotation = random.scaled(-45, 45);
   const start = (sideLength / 2) * -1;
   const stacks = Math.ceil(canvas.width / xDistance) + 1;
@@ -28,13 +28,17 @@ export const sketch: Sketch = async ({ canvas, seed }) => {
   ctx.rotate(degreesToRadians(rotation));
 
   [...new Array(stacks)].forEach((_, i) => {
-    const palette = new Colour({ hue: random.degrees() })
-      .saturate(random.scaled(-10, 40))
-      .decreaseOpacity(random.scaled(0, 0.3))
-      .createComplementaryPalette();
+    const colour = new Colour({
+      hue: random.degrees(),
+      saturation: random.scaled(40, 80),
+      lightness: random.scaled(30, 60),
+      opacity: 0.7,
+    });
     [...new Array(boxes)].forEach((_, j) => {
-      const cos = Math.cos(degreesToRadians(startAngle + j * 4)) * sideLength;
-      const sin = Math.sin(degreesToRadians(startAngle + j * 4)) * sideLength;
+      const cos =
+        Math.cos(degreesToRadians(startAngle + j * 7)) * sideLength + i * 10;
+      const sin =
+        Math.sin(degreesToRadians(startAngle + j * 7)) * sideLength + i * 10;
       const x = start + xDistance * i;
       const y = start + yDistance * j;
       ctx.beginPath();
@@ -43,10 +47,9 @@ export const sketch: Sketch = async ({ canvas, seed }) => {
       ctx.lineTo(x + cos + sin, y + sin + cos);
       ctx.lineTo(x + sin, y + cos);
       ctx.closePath();
-      const selectedColor = palette.selectRandom();
-      ctx.fillStyle = new Gradient(ctx, selectedColor)
-        .addColour(selectedColor.lighten(random.scaledInt(30, 80)), 1)
-        .spreadOver(sideLength * 1.5)
+      ctx.fillStyle = new Gradient(ctx, colour)
+        .addColour(colour.lighten(random.scaledInt(30, 80)), 1)
+        .spreadOver(sideLength + i * 15)
         .rotate(random.scaled(15, 75))
         .moveTo([x, y])
         .toCanvasGradient();
@@ -62,6 +65,6 @@ export const sketch: Sketch = async ({ canvas, seed }) => {
 
 export const meta: SketchMeta = {
   sketchName: "15",
-  defaultSeed: "iiaqw",
+  defaultSeed: "gvuzb",
   controls: [],
 };
